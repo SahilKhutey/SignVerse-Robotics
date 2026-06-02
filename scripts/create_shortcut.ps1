a919765b3dc92f561d2b30e75c7ef1ff732fb86b
SahilKhutey <sahilworks27@gmail.com>
2026-06-02 14:19:07 +0530

    feat(startup): add create_shortcut.ps1 to generate Windows desktop launch icon

---
+++ scripts/create_shortcut.ps1
@@ -0,0 +1,13 @@
+# create_shortcut.ps1
+$TargetFile = "c:\Users\User\Documents\SignVerse-Robotics\sign-verse-robotics\Launch_SignVerse.bat"
+$ShortcutFile = "C:\Users\User\Desktop\SignVerse OS.lnk"
+
+# Create COM Object for Windows Script Host Shell
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.WorkingDirectory = "c:\Users\User\Documents\SignVerse-Robotics\sign-verse-robotics"
+$Shortcut.IconLocation = "imageres.dll, 102" # Microchip / hardware device icon
+$Shortcut.Save()
+
+Write-Host "Success: Desktop shortcut 'SignVerse OS' created at $ShortcutFile" -ForegroundColor Green
